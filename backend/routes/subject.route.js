cb3b3a7ddf21d92557f3eb3b3e817260e58e315c
KaikyNonato <kaikynonato288br@gmail.com>
2026-03-13 20:11:04 -0300

    Add token verification to subject routes

--- backend/routes/subject.route.js
+++ backend/routes/subject.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer';
-import { createSubject, deleteSubject, getSubjects, getAllSubjects, reorderSubjects, updateSubject, uploadAttachment, removeAttachment, concludedReview } from '../controllers/subject.controller.js';
+import { createSubject, deleteSubject, getSubjects, getAllSubjects, reorderSubjects, updateSubject, uploadAttachment, removeAttachment, concludedReview, streamPdf } from '../controllers/subject.controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
@@ -8,17 +8,19 @@ const router = express.Router();
 // Configurando o multer para salvar temporariamente em memória
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post("/create-subject", createSubject);
+router.post("/create-subject",verifyToken, createSubject);
 router.get("/get-subjects", verifyToken, getAllSubjects);
-router.get("/get-subjects/:matter_id", getSubjects);
-router.put("/update-subject/:id", updateSubject);
-router.delete("/delete-subject/:id", deleteSubject);
-router.put('/reorder-subjects', reorderSubjects);
-router.put('/concluded-review/:id/:review', concludedReview);
+router.get("/get-subjects/:matter_id",verifyToken, getSubjects);
+router.put("/update-subject/:id",verifyToken, updateSubject);
+router.delete("/delete-subject/:id",verifyToken, deleteSubject);
+router.put('/reorder-subjects',verifyToken, reorderSubjects);
+router.put('/concluded-review/:id/:review',verifyToken, concludedReview);
 
 
 // NOVAS ROTAS DE ARQUIVO
-router.post("/:id/upload", upload.single('file'), uploadAttachment);
-router.put("/remove-file/:id", removeAttachment);
+router.post("/:id/upload",verifyToken, upload.single('file'), uploadAttachment);
+router.put("/remove-file/:id",verifyToken, removeAttachment);
+
+router.get("/stream-pdf/:subjectId/:publicId", verifyToken, streamPdf);
 
 export default router;
