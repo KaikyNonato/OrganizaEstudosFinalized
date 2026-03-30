ae8f68dd4ae142d01148feac17a080ceaaffbea2
KaikyNonato <kaikynonato288br@gmail.com>
2026-03-30 16:18:36 -0300

    Update cookie settings based on environment

--- backend/utils/genereteTokenAndSetCookie.js
+++ backend/utils/genereteTokenAndSetCookie.js
@@ -5,8 +5,9 @@ export const generateTokenAndSetCookie = (res, userId) => {
 
     res.cookie('token', token, {
         httpOnly: true,
-        secure: true, // DEVE ser true se sameSite for 'none' (obriga HTTPS)
-        sameSite: 'none', // Permite que o cookie viaje entre o frontend e o backend em domínios diferentes
+        // No localhost (dev) = false. No servidor online (prod) = true.
+        secure: process.env.NODE_ENV === "production", 
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", 
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 dias
     });
 
